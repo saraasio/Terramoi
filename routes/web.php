754816812415f4a053baa0908ac11e3adb45b8c4
Aichir0 <evan.moussi.sio@gmail.com>
2026-09-23 15:10:27 +0200

    c1

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sites', function () {
+    return view('bienvenue sur le site');
+});
+
+
+Route::get('/sites/parcelles', function () {
+    return view('bienvenue sur la liste des parcelles ');
+});
+
+Route::get('/sites/parcelles/parcelle/{num}', function ( $num ) {
+    return view('Voici la parcelle n° $num : ');
+});
+
